f710e0c528d6e6fe353890780dcac4f9937cf448
programer10 <programer10@ucione.local>
2022-03-28 20:09:15 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Messages from "./components/Messages";
+import Input from "./components/Input";
+import nouns from "./data/nouns";
+import adjectives from "./data/adjectives";
+
+function randomName() {
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return adjective + noun;
+}
+
+function randomColor() {
+  return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
+}
 
 function App() {
+  const [user, setUser] = useState({
+    username: randomName(),
+    randomColor: randomColor()
+  });
+  const [messages, setMessages] = useState([]);
+  const [drone, setDrone] = useState();
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    const drone = new window.Scaledrone("JGmqrCsbmbemwbQe", {
+      data: user,
+    });
+    setDrone(drone);
+    // eslint-disable-next-line
+  }, []);
+  if (drone) {
+    drone.on("open", (error) => {
+      if (error) {
+        console.log("Error on connecting", error);
+      }
+
+      const chatRoom = drone.subscribe("observable-room");
+
+      chatRoom.on("open", (error) => {
+        if (error) {
+          return console.error(error);
+        }
+        // Connected to room
+      });
+
+      chatRoom.on("data", (text, chatUser) => {
+         setUsers(drone.clientId);
+        
+        const username = chatUser.clientData.username;
+        const chatUserID = chatUser.id;
+        const userColor = chatUser.clientData.randomColor
+        
+        setMessages((oldArray) => [
+          ...oldArray,
+          { text, username, userColor, chatUserID, user },
+        ]);
+      });
+    });
+  }
+
+  const onSendMessage = (message) => {
+    if (message) {
+      drone.publish({
+        room: "observable-room",
+        message,
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>My Chat App</h1>
+      </div>
+      <Messages messages={messages} users={users}/>
+      <Input onSendMessage={onSendMessage} />
     </div>
   );
 }
