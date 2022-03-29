8350fac6d50c00f73023597d10a648012bb93073
programer10 <programer10@ucione.local>
2022-03-29 20:28:03 +0200

    Included emoticons

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Icon from '@material-ui/core/Icon';
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -23,8 +25,16 @@ const Input = ({ onSendMessage }) => {
     onSendMessage(textInputs);
   }
 
+  function addEmoji(e) {
+    let emoji = e.native;
+    setTextInputs(textInputs + emoji)
+  }
+
   return (
     <div className="Input">
+      <span>
+        <Picker onSelect={addEmoji} />
+      </span>
       <form onSubmit={(e) => onSubmit(e)}>
         <input
           onChange={(e) => onChange(e)}
